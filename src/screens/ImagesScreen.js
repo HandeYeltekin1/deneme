0a3c9eb2f6272d146c39de7ebd6cb84e7486ed06
Hande <handeyeltekin@icloud.com>
2023-01-17 17:06:38 +0300

    ImageScreend and ImageDetail pages okey.

--- src/screens/ImagesScreen.js
+++ src/screens/ImagesScreen.js
@@ -8,14 +8,22 @@ import {
   navigation,
   ImageBackground,
 } from "react-native";
-const ImagesScreen = (navigation) => {
-  const image = {
-    url: "https://www.google.com/search?q=mountain&client=safari&rls=en&sxsrf=AJOqlzUSRxSZvXADufvXjoADFeYEZcmQJQ:1673940848309&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi9vo3Ni878AhVNVPEDHRbsDpsQ_AUoAXoECAEQAw&biw=1920&bih=992&dpr=1#imgrc=_piKIIEbENzkfM",
-  };
+import ImageDetail from "../components/ImageDetail";
+const ImagesScreen = () => {
   return (
     <View>
-      <Button onPress={() => navigation.navigate("Home")} title="Forest" />
-      <ImageBackground source={image}></ImageBackground>
+      <ImageDetail
+        title="Forest"
+        imageSource={require("../../assets/forest.jpg")}
+      />
+      <ImageDetail
+        title="Beach"
+        imageSource={require("../../assets/beach.jpg")}
+      />
+      <ImageDetail
+        title="Mountain"
+        imageSource={require("../../assets/mountain.jpg")}
+      />
     </View>
   );
 };
