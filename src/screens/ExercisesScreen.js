47d666acb6d6cb115b85987e369ab68c132f8f06
Hande <handeyeltekin@icloud.com>
2023-01-18 16:12:13 +0300

    useReducer exercises

--- src/screens/ExercisesScreen.js
+++ src/screens/ExercisesScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 
 //1. EGZERSİZ
 // import { Text, StyleSheet, View } from "react-native";
@@ -56,29 +56,71 @@ import React from "react";
 // });
 
 // 3. EGZERSİZ
-import { View, StyleSheet, Text, Image } from "react-native";
-import ExercisesComponent from "../components/ExercisesComponent";
+// import { View, StyleSheet, Text, Image } from "react-native";
+// import ExercisesComponent from "../components/ExercisesComponent";
+// const ExercisesScreen = () => {
+//   return (
+//     <View>
+//       <ExercisesComponent
+//         title="Forest"
+//         Text="ImageScore : 9"
+//         imageSource={require("../../assets/forest.jpg")}
+//       />
+//       <ExercisesComponent
+//         title="Beach"
+//         Text="Image Score : 7"
+//         imageSource={require("../../assets/beach.jpg")}
+//       />
+//       <ExercisesComponent
+//         title="Mountain"
+//         Text="Image Score : 4"
+//         imageSource={require("../../assets/mountain.jpg")}
+//       />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({});
+// export default ExercisesScreen;
+
+// 4. EGZERSİZ
+
+import React, { useReducer } from "react-native";
+import { Button, View, StyleSheet, Text } from "react-native";
+const COUNTER_INC = 1;
+const reducer = (state, action) => {
+  // state ={counter : number}
+  // action ={selectToAct : Increase || decrease , count : 1 || -1 }
+  switch (action.selectToAct) {
+    case "Decrease":
+      if (state.counter != 0) {
+        return { ...state, counter: state.counter + action.count };
+      }
+    case "Increase":
+      return { ...state, counter: state.counter + action.count };
+    default:
+      return state;
+  }
+};
 const ExercisesScreen = () => {
+  const [state, dispatch] = useReducer(reducer, { counter: 1 });
   return (
     <View>
-      <ExercisesComponent
-        title="Forest"
-        Text="ImageScore : 9"
-        imageSource={require("../../assets/forest.jpg")}
+      <Text>Exercises Screen</Text>
+      <Button
+        title="Increase "
+        onPress={() => {
+          dispatch({ selectToAct: "Increase", count: COUNTER_INC });
+        }}
       />
-      <ExercisesComponent
-        title="Beach"
-        Text="Image Score : 7"
-        imageSource={require("../../assets/beach.jpg")}
-      />
-      <ExercisesComponent
-        title="Mountain"
-        Text="Image Score : 4"
-        imageSource={require("../../assets/mountain.jpg")}
+      <Button
+        title="Decrease"
+        onPress={() => {
+          dispatch({ selectToAct: "Decrease ", count: -1 * COUNTER_INC });
+        }}
       />
     </View>
   );
 };
-
 const styles = StyleSheet.create({});
 export default ExercisesScreen;
